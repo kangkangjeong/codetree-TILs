67028e0b8ae3029fd6c89125440199b315d07d56
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-10 10:43:41 +0900

    Title: 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240510/최댓값 구하기/maximum-value.py
+++ 240510/최댓값 구하기/maximum-value.py
@@ -1,10 +1,6 @@
-a,b,c=map(int,input().split(' '))
-def abc(a,b,c):
-    d=list()
-    d.append(a)
-    d.append(b)
-    d.append(c)
-    d.sort(reverse=True)
-    print(d[0])
+a, b, c = map(int, input().split(' '))
 
-abc(a,b,c)
+def abc(a, b, c):
+    print(max(a, b, c))
+
+abc(a, b, c)
