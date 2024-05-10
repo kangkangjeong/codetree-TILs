64d1000b39ab8a1039f6ca0a33332472ae579b81
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-10 10:05:19 +0900

    Title: 두 사람, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240510/두 사람/two-person.py
+++ 240510/두 사람/two-person.py
@@ -1,5 +1,10 @@
-age1,sex1=input().split()
-age2,sex2=input().split()
+age1, sex1 = input().split()
+age2, sex2 = input().split()
+age1 = int(age1)
+age2 = int(age2)
 
-if age1 or age2 >=19 and sex1 or sex2 =='M':
+# 두 사람 중 한 사람이라도 19세 이상이며, 그 사람이 남자인 경우를 정확히 확인
+if ((age1 >= 19 and sex1 == 'M') or (age2 >= 19 and sex2 == 'M')):
     print(1)
+else:
+    print(0)
