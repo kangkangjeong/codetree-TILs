86a3eb8e2b6731909303a105ae0a6bc07e396a43
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-18 01:46:37 +0900

    Title: 알파벳 출력하기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240518/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,14 @@
+n=int(input())
+x=65
+for i in range(n):
+    for j in range(n):
+        if i<=j:
+            if x>89:
+                x=65
+            else:
+                print(chr(x),end=' ')
+                x+=1
+        else:
+            print(' ',end=' ')
+
+    print()
