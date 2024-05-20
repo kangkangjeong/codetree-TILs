c324a05f5112ad64b3c9aab50402667c83e38dcb
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-20 11:29:41 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240520/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240520/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,10 +1,16 @@
-n=int(input())
-m=list(map(int,input().split()))
-cnt=0
-for i in range(n):
-    if cnt==3:
-        break
-    if m[i]==2:
-        cnt+=1
+n = int(input())
+m = list(map(int, input().split()))
+cnt = 0
+i = 0
 
-print(i)
+for i in range(n):
+    if cnt == 3:
+        break
+    if m[i] == 2:
+        cnt += 1
+
+# cnt가 3에 도달했는지 확인
+if cnt == 3:
+    print(i)
+else:
+    print("2가 3번 존재하지 않습니다.")
