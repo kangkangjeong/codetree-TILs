f1e791ead487d13465aa4ebfb632fe181d7b8f59
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-20 11:31:58 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240520/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240520/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -4,13 +4,12 @@ cnt = 0
 i = 0
 
 for i in range(n):
-    if cnt == 3:
-        break
+
     if m[i] == 2:
         cnt += 1
+    if cnt == 3:
+        break
 
 # cnt가 3에 도달했는지 확인
 if cnt == 3:
-    print(i)
-else:
-    print("2가 3번 존재하지 않습니다.")
+    print(i+1)
