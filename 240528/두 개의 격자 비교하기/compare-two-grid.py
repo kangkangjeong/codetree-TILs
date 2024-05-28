ee1c91ab9afafb7f0a8342af350dac29cd3254ce
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-28 11:27:58 +0900

    Title: 두 개의 격자 비교하기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240528/두 개의 격자 비교하기/compare-two-grid.py
+++ 240528/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,19 +1,21 @@
-n,m=map(int,input().split())
-arr2d_1=[
-    list(map(int,input().split()))
-    for _ in range(m)
+n, m = map(int, input().split())
+arr2d_1 = [
+    list(map(int, input().split()))
+    for _ in range(n)  # n으로 변경
 ]
-arr2d_2=[
-    list(map(int,input().split()))
-    for _ in range(m)
+arr2d_2 = [
+    list(map(int, input().split()))
+    for _ in range(n)  # n으로 변경
 ]
-arr2d_new=[
-    [1 for _ in range(m)]
+arr2d_new = [
+    [1 for _ in range(m)]  # 초기값을 0으로 설정
     for _ in range(n)
 ]
-for index1,i in enumerate(arr2d_1):
-    for index2,j in enumerate(arr2d_2):
-        if arr2d_1[index1][index2]==arr2d_2[index1][index2] :
-            arr2d_new[index1][index2]=0
-for k in (arr2d_new):
-    print(*k)
+
+for index1 in range(n):
+    for index2 in range(m):
+        if arr2d_1[index1][index2] == arr2d_2[index1][index2]:
+            arr2d_new[index1][index2] = 0
+
+for row in arr2d_new:
+    print(*row)
