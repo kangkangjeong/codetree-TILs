8aef05b8428e1d094a222156f597e7cd081158e7
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-05-31 15:14:57 +0900

    Title: 가장 짧은 문자열, Time: 121ms, Memory: 73MB, Status: Passed - CodeTree

--- 240531/가장 짧은 문자열/shortest-string.py
+++ 240531/가장 짧은 문자열/shortest-string.py
@@ -1,16 +1,15 @@
-text_list=[]
+text_list = []
 for _ in range(3):
     text_list.append(input())
 
-for i in range(3):
-    if len(text_list[i])>len(text_list[0]):
-        ma=len(text_list[i])
-    else:
-        ma=len(text_list[0])
-for j in range(3):
-    if len(text_list[j])<len(text_list[0]):
-        mi=len(text_list[j])
-    else:
-        mi=len(text_list[0])
+# 초기값 설정 시 첫 번째 요소의 길이를 사용
+ma = len(text_list[0])
+mi = len(text_list[0])
 
-print(ma-mi)
+for i in range(3):
+    if len(text_list[i]) > ma:
+        ma = len(text_list[i])
+    if len(text_list[i]) < mi:
+        mi = len(text_list[i])
+
+print(ma - mi)
