a396de7d7911af6f1458d3b676af4e725eeb99f6
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-09-26 12:00:55 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240926/시력 검사 2/eye-test-2.c
+++ 240926/시력 검사 2/eye-test-2.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int num;
-    scanf("%f",&num);
-    if(num>=1.0)
-        printf("High");
-    else if(num>=0.5)
-        printf("Middle");
+    float num;  // num을 float 타입으로 변경
+
+    printf("숫자를 입력하세요: ");
+    scanf("%f", &num);  // %f로 수정
+
+    if (num >= 1.0)
+        printf("High\n");
+    else if (num < 1.0 && num >= 0.5)  // 조건 수정
+        printf("Middle\n");
     else
-        printf("Low");
+        printf("Low\n");
+
     return 0;
 }
