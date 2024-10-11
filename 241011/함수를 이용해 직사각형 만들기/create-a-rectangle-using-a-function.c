44d3fe97b4408588d00244fcb52a90e4def36ddb
JeongHun Kang <126036162+kangkangjeong@users.noreply.github.com>
2024-10-11 13:52:08 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241011/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_rect(int n, int m) {
+    for (int i = 0; i < n; i++) { // 조건을 i < n으로 수정
+        for (int j = 0; j < m; j++) { // m에 대한 반복 추가
+            printf("1");
+        }
+        printf("\n"); // 각 행의 끝에서 줄 바꿈
+    }
+}
+
+int main() {
+    print_rect(2, 3);
+    return 0;
+}
